19d7622adb1c99ce59af311134a10c2e9e35ab80
dalmook <94118801+dalmook@users.noreply.github.com>
2025-05-16 20:07:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Google Apps Script 웹앱 URL
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwLUmNjZT7Gw0iQQ46nrR3oyQBm5qDEy-mSlPMBy9mhZp448KKaM3Sae5FkSW39-nJsCA/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbywg-XX55lWqELMjYYmVD911gLNUwssh33FzC1XN8S4yTKqHiKGSZA2B5gONn34oC7s/exec';
 
 document.addEventListener('DOMContentLoaded', () => {
   const walkInInput = document.getElementById('walkInTime');
